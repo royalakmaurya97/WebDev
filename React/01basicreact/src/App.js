3a15abe2272622c1ccf03189d6b190b339a74792
Alok Kumar <mauryaak13@gmail.com>
2025-11-02 10:52:00 +0530

    Normalize line endings (apply .gitattributes) and add 01basicreact as regular folder

---
+++ React/01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+
+    <>
+    <Chai/>
+    <h1>Chai Aur React | Alok Maurya</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ React/01basicreact/src/Chai.jsx
@@ -0,0 +1,10 @@
+
+
+
+function Chai() {
+    return (
+        <h3>Chai With React |</h3>
+    )
+}
+
+export default Chai;
